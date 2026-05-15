9d34db444444322c216b20229cc64d1a0f0478a3
wingeherose9145 <121928673+wingeherose9145@users.noreply.github.com>
2026-05-15 21:15:38 +0800

    Update SplashActivity.kt

---
+++ app/src/main/java/com/system/helper/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.system.helper
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(
+            R.layout.activity_splash
+        )
+
+        Handler(
+            Looper.getMainLooper()
+        ).postDelayed({
+
+            startActivity(
+                Intent(
+                    this,
+                    SplashActivity::class.java
+                )
+            )
+
+            finish()
+
+        }, 1500)
+    }
+}
